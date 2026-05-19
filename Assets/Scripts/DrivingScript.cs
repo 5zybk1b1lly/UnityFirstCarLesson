7ece726355983859a6d1f00ffe3dc033147c7b02
5zybk1b1lly <paluchszymon236@gmail.com>
2026-05-19 18:21:16 +0100

    Jazda i sterowanie
    Utworzenie skryptu odpowiedzialnego za jazdę oraz sterowanie pojazdem

---
+++ Assets/Scripts/DrivingScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivingScript : MonoBehaviour
+{
+    public WheelScript[] wheels;
+    public float torque = 200;
+    public float maxSteerAngle = 30;
+    public float maxBrakeTorque = 500;
+    public float maxSpeed = 150;
+    public Rigidbody rb;
+    public float currentSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Drive(float accel, float brake, float steer)
+    {
+        accel = Mathf.Clamp(accel, -1,1);
+        steer = Mathf.Clamp(steer, -1,1) * maxSteerAngle;
+        brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
+
+        float thrustTorque = 0;
+        if (currentSpeed < maxSpeed) thrustTorque = accel * torque;
+        foreach (WheelScript wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = thrustTorque;
+            if (wheel.isFrontWheel) wheel.wheelCollider.steerAngle = steer;
+            else wheel.wheelCollider.brakeTorque = brake;
+            Quaternion quat;
+            Vector3 position;
+            wheel.wheelCollider.GetWorldPose(out position, out quat);
+            wheel.wheel.transform.position = position;
+            wheel.wheel.transform.rotation = quat;
+        }
+    } 
+}
